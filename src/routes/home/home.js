33375e3f76786b7a3ee0ff4def5e45c16bc3844d
郑聪 <931745526@qq.com>
2018-11-08 19:57:41 +0800

    add routes

---
+++ src/routes/home/home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from './logo.svg'
+import styles from './home.module.less'
+
+const Home = () => {
+  return (
+    <div className={styles.Home}>
+      <header className={styles.HomeHeader}>
+        <img src={logo} className={styles.HomeLogo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className={styles.HomeLink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default Home
