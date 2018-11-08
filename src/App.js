9bd380053c0d4d67f6114e36ac7074195478e69c
郑聪 <931745526@qq.com>
2018-11-08 20:01:49 +0800

    add router in App

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { hot } from 'react-hot-loader'
-import requestApi from 'utils/http'
-import logo from './logo.svg'
-import styles from './App.module.less'
+import { HashRouter, Route } from 'react-router-dom'
+import BasicLayout from 'layouts/BasicLayout'
+import routes from 'config/route.config'
+import navData from 'config/nav.config'
 
-class App extends Component {
-  componentDidMount() {
-    requestApi.post('', {
-      mod: '/user/login',
-    }, {
-      pass: '123',
-      username: 'gepeng',
-    })
-      .then((data) => {
-        console.log('data', data)
-      })
-      .catch((err) => {
-        console.log('error', err)
-      })
-  }
-
-  render() {
-    return (
-      <div className={styles.App}>
-        <header className={styles.AppHeader}>
-          <img src={logo} className={styles.AppLogo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={styles.AppLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <HashRouter>
+      <Route path="/" render={() => <BasicLayout routes={routes} navData={navData} />} />
+    </HashRouter>
+  )
 }
 
 export default hot(module)(App)
