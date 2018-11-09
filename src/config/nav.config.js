22db2cfe980b7f1209fe9a0f773c2719d030bbf5
郑聪 <931745526@qq.com>
2018-11-09 15:33:22 +0800

    delete id

--- src/config/nav.config.js
+++ src/config/nav.config.js
@@ -1,11 +1,9 @@
 const NavConfig = [
   {
-    'id': 'home',
     'text': '首页',
     'icon': 'home',
     'url': '/home',
   }, {
-    'id': 'demo',
     'text': 'demo',
     'icon': 'bars',
     'url': '/demo',
