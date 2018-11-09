42c4825af3dc4450d3856335f28b617e4116d822
郑聪 <931745526@qq.com>
2018-11-09 15:29:26 +0800

    optimize with loadable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import { HashRouter, Route } from 'react-router-dom'
-import BasicLayout from 'layouts/BasicLayout'
 import routes from 'config/route.config'
 import navData from 'config/nav.config'
+import { getLoadableComponent } from 'utils/common'
+
+const LoadableBasicLayout = getLoadableComponent(() => import('layouts/BasicLayout'))
 
 const App = () => {
   return (
     <HashRouter>
-      <Route path="/" render={() => <BasicLayout routes={routes} navData={navData} />} />
+      <Route path="/" render={({ location }) => <LoadableBasicLayout location={location} routes={routes} navData={navData} />} />
     </HashRouter>
   )
 }
