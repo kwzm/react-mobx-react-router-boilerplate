8e97aae26fd4ee2d00fb782b8a862f0ad310f6ed
郑聪 <931745526@qq.com>
2018-11-09 15:32:37 +0800

    add 404 route

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,14 @@
-const config = {
-  api: {
-    dev: {
-      API_HOST: 'http://localhost',
-      API_PATH: '/api/index.php',
-      API_PORT: '3000',
-    },
-    prod: {
-      API_HOST: '',
-      API_PATH: '/api',
-      API_PORT: '80',
-    },
+export const apiConfig = {
+  dev: {
+    API_HOST: 'http://localhost',
+    API_PATH: '/api/index.php',
+    API_PORT: '3000',
+  },
+  prod: {
+    API_HOST: '',
+    API_PATH: '/api',
+    API_PORT: '80',
   },
 }
 
-export default config
+export default apiConfig
--- src/config/route.config.js
+++ src/config/route.config.js
@@ -3,10 +3,12 @@ import { Route, Redirect } from 'react-router-dom'
 import { getLoadableComponent } from 'utils/common'
 
 const LoadedHome = getLoadableComponent(() => import('routes/home'))
+const Loaded404 = getLoadableComponent(() => import('routes/404'))
 
 const routes = [
-  <Route path="/" render={() => <Redirect to="/home" />} exact key="first" />,
+  <Route path="/" exact render={() => <Redirect to="/home" />} key="first" />,
   <Route path="/home" component={LoadedHome} key="home" />,
+  <Route component={Loaded404} key="404" />,
 ]
 
 export default routes
