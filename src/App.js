d0879a3281b5a66a053e9e5ff868b591a2d83cd0
郑聪 <931745526@qq.com>
2018-11-14 10:39:55 +0800

    add mobx provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import { HashRouter, Route } from 'react-router-dom'
+import { Provider } from 'mobx-react'
 import routes from 'config/route.config'
 import navData from 'config/nav.config'
 import { getLoadableComponent } from 'utils/common'
+import stores from './stores'
 
 const LoadableBasicLayout = getLoadableComponent(() => import('layouts/BasicLayout'))
 
 const App = () => {
   return (
-    <HashRouter>
-      <Route path="/" render={({ location }) => <LoadableBasicLayout location={location} routes={routes} navData={navData} />} />
-    </HashRouter>
+    <Provider {...stores}>
+      <HashRouter>
+        <Route
+          path="/"
+          render={({ location }) => (
+            <LoadableBasicLayout location={location} routes={routes} navData={navData} />
+          )}
+        />
+      </HashRouter>
+    </Provider>
   )
 }
 
