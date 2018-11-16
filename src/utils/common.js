b5d98893cfb7993c51c36f15f830e295ac1f8221
郑聪 <931745526@qq.com>
2018-11-16 16:00:01 +0800

    add isIe9

--- src/utils/common.js
+++ src/utils/common.js
@@ -5,11 +5,22 @@ export const getIsDev = () => {
   return process.env.NODE_ENV === 'development'
 }
 
-export const getLoadableComponent = (loader) => {
+export const getLoadableComponent = loader => {
   return Loadable({
     loader,
     loading: LoadingInLoadable,
   })
+}
+
+export const isIe9 = () => {
+  if (
+    navigator.appName === 'Microsoft Internet Explorer' &&
+    navigator.appVersion.split(';')[1].replace(/[ ]/g, '') === 'MSIE9.0'
+  ) {
+    return true
+  }
+
+  return false
 }
 
 export default getIsDev
