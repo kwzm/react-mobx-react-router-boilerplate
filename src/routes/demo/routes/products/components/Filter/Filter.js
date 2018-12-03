d7931be5796b5730e80c7ab401db386c61b4967c
郑聪 <931745526@qq.com>
2018-12-03 18:04:11 +0800

    search when input onPressEnter

--- src/routes/demo/routes/products/components/Filter/Filter.js
+++ src/routes/demo/routes/products/components/Filter/Filter.js
@@ -16,6 +16,18 @@ class Filter extends React.Component {
     this.props.handleSearch()
   }
 
+  handlePressEnter = (e, key) => {
+    this.props.handleSearch({
+      [key]: e.target.value,
+    })
+  }
+
+  handleChange = (val, key) => {
+    this.props.handleSearch({
+      [key]: val,
+    })
+  }
+
   render() {
     const {
       form: { getFieldDecorator },
@@ -31,19 +43,26 @@ class Filter extends React.Component {
         <Row gutter={40}>
           <Col span={8}>
             <FormItem label="名称" {...formItemLayout}>
-              {getFieldDecorator('name')(<Input />)}
+              {getFieldDecorator('name')(
+                <Input onPressEnter={(e) => this.handlePressEnter(e, 'name')} />
+              )}
             </FormItem>
           </Col>
           <Col span={8}>
             <FormItem label="生产商" {...formItemLayout}>
-              {getFieldDecorator('manufacturer')(<Input />)}
+              {getFieldDecorator('manufacturer')(
+                <Input onPressEnter={(e) => this.handlePressEnter(e, 'manufacturer')} />
+              )}
             </FormItem>
           </Col>
           <Col span={8}>
             <FormItem label="类别" {...formItemLayout}>
               {getFieldDecorator('category')(
-                <Select placeholder="请选择类别">
-                  {categories.map(item => (
+                <Select
+                  placeholder="请选择类别"
+                  onChange={(val) => this.handleChange(val, 'category')}
+                >
+                  {categories.map((item) => (
                     <Option key={item.value} value={item.value}>
                       {item.name}
                     </Option>
@@ -56,12 +75,21 @@ class Filter extends React.Component {
         <Row gutter={40}>
           <Col span={8}>
             <FormItem label="生产日期" {...formItemLayout}>
-              {getFieldDecorator('productionDate')(<DatePicker />)}
+              {getFieldDecorator('productionDate')(
+                <DatePicker onChange={(val) => this.handleChange(val, 'productionDate')} />
+              )}
             </FormItem>
           </Col>
           <Col span={8}>
             <FormItem label="价格" {...formItemLayout}>
-              {getFieldDecorator('price')(<InputNumber min={0.0} precision={2} step={0.01} />)}
+              {getFieldDecorator('price')(
+                <InputNumber
+                  min={0.0}
+                  precision={2}
+                  step={0.01}
+                  onChange={(val) => this.handleChange(val, 'price')}
+                />
+              )}
             </FormItem>
           </Col>
           <Col span={8}>
@@ -88,8 +116,5 @@ export default Form.create({
       price: { value: data.price },
       category: { value: data.category },
     }
-  },
-  onValuesChange: (props, values) => {
-    props.handleSearch(values)
   },
 })(Filter)
