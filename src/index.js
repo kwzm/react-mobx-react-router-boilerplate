510d26d5397289abed2897cba444c999d09dd788
郑聪 <931745526@qq.com>
2018-12-06 16:36:11 +0800

    do not package mock

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import moment from 'moment'
 import 'moment/locale/zh-cn'
-import { getIsDev, isMock } from '@/utils/common'
 import 'minireset.css'
 import './index.css'
 import App from './App'
 
-if (getIsDev() && isMock()) {
+if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_MOCK !== 'none') {
   import('../mock')
 }
 
