c475492780d89006b72adfd75ed190baf1a3b4d3
郑聪 <931745526@qq.com>
2019-04-23 09:43:01 +0800

    modify class name

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import logo from './logo.svg'
-import styles from './home.module.less';
+import styles from './home.module.less'
 
 const Home = () => {
   return (
-    <div className={styles.Home}>
-      <header className={styles.HomeHeader}>
-        <img src={logo} className={styles.HomeLogo} alt="logo" />
+    <div className={styles.home}>
+      <header className={styles.homeHeader}>
+        <img src={logo} className={styles.homeLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className={styles.HomeLink}
+          className={styles.homeLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
